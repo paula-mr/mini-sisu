789c2c198fc1bd1ed040045abdcaef6091ebdeeb
Paula Ribeiro <paula.mribeiro05@gmail.com>
2019-05-01 22:52:48 -0300

    Remove assinaturas de função da main

--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 
 #include "Orquestrador.h"
 
-Lista<Curso>* lerCursos(int quantidadeCursos);
-Lista<Pessoa>* lerPessoas(int quantidadeAlunos, Lista<Curso>* cursos);
-void calcularAprovados(Lista<Curso>* cursos, Lista<Pessoa>* pessoas);
-void removerDuplicacaoAprovados(Lista<Curso>* cursos, Lista<Pessoa>* pessoas);
-
-void imprimirCursos(Lista<Curso>* cursos);
-void imprimirPessoas(Lista<Pessoa>* pessoas);
-
 int main() {
     std::cout << std::fixed;
     std::cout << std::setprecision(2);
